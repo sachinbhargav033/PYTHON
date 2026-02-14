43a63f37136ea1d8156a8d987d2d93d055584088
SACHINBHARGAV <sachinbhargav033@gmail.com>
2026-02-13 19:43:22 -0800

    Add files via upload

---
+++ Day2-IO operation and type converstion..py
@@ -0,0 +1,39 @@
+# read input number from user
+num=input()
+print(type(num))# checking the type of number
+print(num)"""
+
+"""
+#implecit type conversion
+
+num1=int(input())
+val = flot(input())
+print(type(num1), type(val))# checking the type of number
+res = num1+ val
+print(res)
+"""
+
+"""
+num= input()#read string type number
+print(type(num1)) # checking the type of number
+res = num1 + 10 #typeerror can only concatenate str (not "int") to str
+print(res)
+"""
+
+"""
+#To overcome the above pormblem by using explicit type covesrsion
+num= input()#read string type number
+print(type(num1)) # checking the type of number
+res = num1 + 10 #typeerror can only concatenate str (not "int") to str
+print(res)
+"""
+
+"""
+#coverting integer to flot,string and boolen
+num=12
+print(flot(num))
+print(str(num))
+print(bool(num))
+
+
+
